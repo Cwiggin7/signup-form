f70f40e35da09762148c9e3026ac956cd0fe00e6
unknown <cwiggin7@ksu.edu>
2022-03-31 10:22:05 -0500

    created form - connor

---
+++ script.js
@@ -0,0 +1,9 @@
+var myInput = document.getElementById('password');
+
+myInput.onkeyup = function() {
+    if(myInput.value.match(document.getElementById('confirm_password').value) {
+
+    } else {
+
+    }
+}
