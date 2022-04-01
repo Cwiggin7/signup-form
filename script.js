87acbbf6b26af5ae20d4455d48eeaf3fb54a261a
Connor <cwiggin7@ksu.edu>
2022-03-31 20:44:46 -0500

    formatted form - connor

--- script.js
+++
@@ -1,9 +0,0 @@
-var myInput = document.getElementById('password');
-
-myInput.onkeyup = function() {
-    if(myInput.value.match(document.getElementById('confirm_password').value) {
-
-    } else {
-
-    }
-}
