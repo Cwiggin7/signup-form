808975c4d39e1c6c62e84f45e11a6eb28be38c78
Connor <cwiggin7@ksu.edu>
2022-04-15 00:29:36 -0500

    Form method changed to post - Connor

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-document.getElementById('password').addEventListener('change', enableSubmit);
-document.getElementById('confirm_password').addEventListener('change', enableSubmit);
+document.getElementById('password').addEventListener('input', enableSubmit)
+document.getElementById('confirm_password').addEventListener('input', enableSubmit)
 
 document.getElementById('submit').onclick = function() {
     if(document.getElementById('password').value !=
     document.getElementById('confirm_password').value){
-        this.disabled = true;
-        document.getElementById('message').textContent="passwords not matching";
+        this.disabled = true
+        document.getElementById('message').textContent="passwords not matching"
     }
 }
 
 function enableSubmit() {
-    document.getElementById('submit').disabled = false;
+    document.getElementById('submit').disabled = false
 }
